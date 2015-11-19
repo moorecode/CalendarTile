f27bd9a238ccb6f379816eda7873c59c6aedeadd
Sam Moore <sam.moore31@gmail.com>
2015-11-20 02:02:38 +1100

    Initial commit

---
+++ CalendarTileExample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CalendarTileExample
+//
+//  Created by Sam Moore on 20/11/2015.
+//  Copyright © 2015 MooreCode. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
